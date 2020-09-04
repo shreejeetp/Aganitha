46b2abe818b00cdaa98065ae6b6ac550d842b339
shreejeetp <Praveen.shreejeet@gmail.com>
2020-09-04 12:54:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+
+def con1(para):
+  result=re.match('(\d)( dollars)',para)
+  para=para.replace(result.string,'$'+result.group(1))
+  print(para)
+  return para
+def con2(para):
+  result=re.match('C M',para)
+  if(result!=None):
+    para=para.replace(result.string,'CM')
+  return para
+def con3(para):
+  result=re.match('(triple) (/w)',para)
+  if(result!=None):
+    para=para.replace(result.string,result.group(2)*3)
+  return para
+def converter1(para):
+  print(para)
+  para=con1(para)
+  para=con2(para)
+  para=con3(para)
+  return para
+
+para="2 dollars C M triple b"
+res=converter1(para)
+print(res)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name=’Aganitha_test’,
+    packages=find_packages(include=[‘Aganitha_test’]),
+    version=’0.1.0',
+    description=’..’,
+    author=’Shreejeet praveen’,
+    license=’Sp’,
+)
